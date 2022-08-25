64e57e0af58fa2aea8bc1c603ece854119f7b3ec
Mateo Perez <mateojavierperez.mjp@gmail.com>
2022-08-24 23:51:39 -0300

    js

---
+++ index.js
@@ -0,0 +1,18 @@
+// let nombre = prompt("ingrese su nombre");
+
+// while(nombre != "mateo")
+// {
+//     alert("usted no tiene acceso");
+//     nombre = prompt("ingrese su nombre");
+// }
+
+// alert("usted tiene acceso al sistema");
+
+
+for (let a = 0; a < 10; a++){
+    let nombre = prompt ("ingrese su nombre");
+    let mensaje = nombre + " su turno es " + a;
+    alert (mensaje);
+}
+
+alert("no hay mas turnos")
